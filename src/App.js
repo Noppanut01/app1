ca5292430add01662345cada0eb8c1b780681025
Noppanut <noppanut.jang2547@gmail.com>
2025-08-04 10:59:29 +0700

    event inspect

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import { Header, Content, Footer } from "./func-components"
 import Calender from "./class-components"
 import Button from "./class-components"
 import { Calculator2 } from "./calculator"
+import { EventData2 } from "./event-data"
 
 function App() {
-  return <Calculator2 />
+  return <EventData2 />
 }
 
 export default App
