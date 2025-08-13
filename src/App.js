3a17586259a04a015bcb5adc506ce091c7bf023e
Noppanut <noppanut.jang2547@gmail.com>
2025-08-13 14:09:37 +0700

    useRefs Array

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import React from "react"
-import { Header, Content, Footer } from "./func-components"
-import Calender from "./class-components"
-import Button from "./class-components"
-import { Calculator2 } from "./calculator"
-import { EventData2 } from "./event-data"
+import RefsArray from "./refs-array"
 
 function App() {
-  return <EventData2 />
+  return <RefsArray />
 }
 
 export default App
